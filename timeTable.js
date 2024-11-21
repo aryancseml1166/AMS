ab64dde36bb9f7de9403141c936efd7c46be0df8
Aryan Dev Tyagi <aryan.2327cseml1166@kiet.edu>
2024-11-21 19:32:28 +0530

    Add files via upload

---
+++ timeTable.js
@@ -0,0 +1,150 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'H-602',
+        subject: 'BCS 303',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'H-602',
+        subject: 'BAS 301',
+        type: 'Tutorial'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'H-602',
+        subject: 'BVE 301',
+        type: 'Lecture'
+    },
+    {   
+        time: '1-2 PM',
+        roomNumber: '-',
+        subject: 'Break',
+        type: '-'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'H-602',
+        subject: 'BAS 301',
+        type: 'Tutorial'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'H-602',
+        subject: 'BCC 302',
+        type: 'Lecture'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'H-602',
+        subject: 'BCC 302',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'H-602',
+        subject: 'BVE 301',
+        type: 'Lecture'
+    },
+    {   
+        time: '1-2 PM',
+        roomNumber: '-',
+        subject: 'Break',
+        type: '-'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: 'H-602',
+        subject: 'BCS 301',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'H-602',
+        subject: 'BCS 301',
+        type: 'Lecture'
+    },
+    {   
+        time: '1-2 PM',
+        roomNumber: '-',
+        subject: 'Break',
+        type: '-'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: 'H-602',
+        subject: 'BAS 301',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'H-602',
+        subject: 'BCC 302',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'H-602',
+        subject: 'BVE 301',
+        type: 'Lecture'
+    },
+    {   
+        time: '1-2 PM',
+        roomNumber: '-',
+        subject: 'Break',
+        type: '-'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: 'H-602',
+        subject: 'BCS 302',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'H-602',
+        subject: 'BCS 302',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'H-602',
+        subject: 'BCC 302',
+        type: 'Tutorial'
+    },
+    {   
+        time: '1-2 PM',
+        roomNumber: '-',
+        subject: 'Break',
+        type: '-'
+    }
+]
+
+const Saturday =[
+    {   
+        time: 'Saturday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
